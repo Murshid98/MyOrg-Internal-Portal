4dbd8f01f6e76af941b95a70897cc127b80ebae1
Murshid98 <abdullamurshidkv@gmail.com>
2026-01-02 12:04:37 +0530

    First Commit

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,5 @@
 import { defineConfig } from 'vite'
+import { resolve } from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -10,5 +11,14 @@ export default defineConfig({
         secure: false, // For non-verified HTTPS if needed, or because we are proxying to https
       }
     }
-  }
+  },
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        ticket: resolve(__dirname, 'it-ticket.html'),
+        confirmation: resolve(__dirname, 'confirmation.html'),
+      },
+    },
+  },
 })
